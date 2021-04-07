ea435dfc99ce9cb214d158ed35e9dbff72821139
Fabi Diartt <fandig27@gmail.com>
2021-04-06 21:30:57 -0500

    Creating simple card view

--- DesignCodeCourse/DesignCodeCourse/ContentView.swift
+++ DesignCodeCourse/DesignCodeCourse/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(.accentColor)
+                }
+                Spacer()
+                Image("Logo1")
+            }.padding(.horizontal)
+            .padding(.top)
+        
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
 
